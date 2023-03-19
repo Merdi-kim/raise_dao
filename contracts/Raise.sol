d1db4892f09cd042d8774b5a6eec319ce38ab2d8
Merdi-kim <merdikimuha1@gmail.com>
2023-03-20 01:47:07 +0300

    add contract initial  functions

---
+++ contracts/Raise.sol
@@ -0,0 +1,69 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract Raise {
+
+    /** @dev Anatomy of a proposal onchain */
+    struct Proposal {
+        uint256 totalAmount;
+        uint256 amountRaised;
+        address payable proposalCreator;
+        uint256 flagCount;
+        bool FundsSent;
+    }
+
+    /** 
+     * @dev Modifier that checks if the funding amount has not been completed.
+     * This is to avoid any project to be overfunded i.e being funded above the specified amount
+      */
+    modifier proposalCheck(string memory _proposalId) {
+        Proposal memory proposal = proposals[_proposalId];
+        require(proposal.amountRaised < proposal.totalAmount, 'Funding done');
+        _;
+    }
+
+
+
+    event ProposalCreated(string proposalId);
+    event ProposalFunded(string proposalId, uint256 amount);
+
+
+    /**@dev List of all proposals already created and recorded onchain */
+    mapping(string => Proposal) proposals;
+
+    function createProposal( string memory _proposalId ) external {
+        Proposal memory newProposal = Proposal(0, 0, payable(msg.sender), 0, false);
+        proposals[_proposalId] = newProposal;
+        emit ProposalCreated(_proposalId);
+    }
+
+    function fundProposal( string memory _proposalId ) external payable proposalCheck(_proposalId) {
+        require(msg.value > 0, 'null amount');
+        payable(address(this)).transfer(msg.value);
+        emit ProposalFunded(_proposalId, msg.value);
+    }
+
+    /**
+     * This function is helpful to notify about malicious projects.
+     * Each project flaged will be investigated further to see if it's legitimate
+     * No restriction for who can flag a project 
+     */ 
+    function flagProposal( string memory _proposalId ) external proposalCheck(_proposalId) {
+        proposals[_proposalId].flagCount = proposals[_proposalId].flagCount + 1;
+    }
+
+    function unlistProposal() external {}  //to be thought about 
+
+    function pauseProposal() external {}  //to be thought about
+
+    /**
+     * Owner of a proposal can withdraw funds when the campain is done
+     */
+    function withdrawFunds( string memory _proposalId ) external {
+        Proposal memory proposal = proposals[_proposalId];
+        require(proposal.amountRaised >= proposal.totalAmount, 'Funding ongoing');
+        proposal.FundsSent = true;
+        proposal.proposalCreator.transfer(proposal.totalAmount);
+    }
+
+}
