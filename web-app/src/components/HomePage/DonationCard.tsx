1a595137b3db178d49fec92f4765ab5a9519bee7
Merdi-kim <merdikimuha1@gmail.com>
2023-03-24 14:40:34 +0300

    polish proposal card

--- web-app/src/components/HomePage/DonationCard.tsx
+++ web-app/src/components/HomePage/DonationCard.tsx
@@ -15,25 +15,34 @@ interface DonationData {
 
 const DonationCard:FC<DonationData> = ({ data }) => {
   return (
-    <div className='w-8/12 h-[16rem] my-4'>
+    <div className='w-8/12 h-[16rem] mb-32'>
       <Link href={{ pathname:`/${data.title}`, query: { id : data.title } }}>
-      <div className='flex items-center rounded-xl bg-gray-200 px-2'>
+      <div className='flex items-center rounded-xl bg-gray-100 shadow-2xl px-2'>
         <img 
           src="https://www.shutterstock.com/image-photo/top-view-diverse-young-volunteers-260nw-1821196490.jpg" 
           alt="" 
-          className='h-[240px] w-[270px] object-cover'
+          className='h-[270px] w-[270px] rounded-lg my-4 object-cover'
         />
-        <div className='h-full p-4'>
+        <div className='h-full w-full text-center p-4'>
             <h3 className='font-bold text-xl mb-4'>{data.title}</h3>
-            <p className='ml-8 font-normal text-normal line-clamp-5'>
+            <p className='ml-8 font-normal text-left text-normal text-gray-800 h-[7.5rem] line-clamp-5'>
                {data.explanation}
             </p>
-            <div className='h-12 bg-gray-300 my-4'>
-                {/*think about the graph to show payment process */}
+            <div className='h-10 flex flex-col items-center justify-center my-4'>
+              <progress className='w-2/4 h-[1rem]' value="75" max="100"/>
+              <div className='font-semibold flex mt-2 text-sm'>
+                <div className='flex items-center'>
+                  3456 <img src="/assets/eth-logo.png" className='h-3' alt="" />
+                </div>
+                 / 
+                <div className='flex items-center'>
+                  34560 <img src="/assets/eth-logo.png" className='h-3' alt="" />
+                </div>
+              </div>
             </div>
-            <div className='flex justify-between'>
+            <div className='flex justify-between pl-8'>
                 <DonorsCount/>
-                <button>Read more</button>
+                <button className='pb-2 border-gray-400 hover:border-b-2'>Read more</button>
             </div>
         </div>
       </div>
--- web-app/src/components/HomePage/DonorsCount.tsx
+++ web-app/src/components/HomePage/DonorsCount.tsx
@@ -4,9 +4,9 @@ const DonorsCount = () => {
   return (
     <div>
         <div className='flex items-center'>
-           <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjbCP8d5uk42S3HJTpsUeexuliJrcepqtDnN4DEtOkM6tlnbKLMqHbSSuZte0E9bjBNw4&usqp=CAU" alt="" className='h-8 w-8 rounded-[50%] bg-gray-400' />
-           <img src="https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="" className='h-8 w-8 rounded-[50%] ml-[-18px] bg-gray-400' />
-           <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9dIm2pJw9ss3AnQ9kFSDYjltejWW_Hhpajmhm9EC8yJy4xETBWCvz9F39cl9GSyBIdm0&usqp=CAU" alt="" className='h-8 w-8 rounded-[50%] ml-[-18px] bg-gray-400'/> 
+           <img src="/assets/user.png" alt="" className='h-8 w-8 rounded-[50%]' />
+           <img src="/assets/user.png" alt="" className='h-8 w-8 rounded-[50%] ml-[-18px] bg-gray-100' />
+           <img src="/assets/user.png" alt="" className='h-8 w-8 rounded-[50%] ml-[-18px] bg-gray-100'/> 
            <div className='text-sm'> +<span className='font-bold'>243</span> others donations</div>
         </div>
     </div>
