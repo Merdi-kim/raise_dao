bbb08c3efcf53c334a634d9ee0e96a61d4c70752
Merdi-kim <merdikimuha1@gmail.com>
2023-03-24 16:31:20 +0300

    polish create proposal page

--- web-app/src/pages/new-proposal.tsx
+++ web-app/src/pages/new-proposal.tsx
@@ -66,23 +66,31 @@ const NewProposal = () => {
   return (
     <div>
       <Navbar/>
-      <div className='w-full min-h-screen flex flex-col items-center pb-12 bg-gray-400'>
-        <form onSubmit={publishData} className='w-7/12 mt-4 pt-4 flex flex-col items-center rounded-xl bg-slate-500'>
+      <div className='w-full min-h-screen flex flex-col items-center justify-center pb-12'>
+        <form onSubmit={publishData} className='w-7/12 mt-4 pt-4 flex flex-col items-center rounded-xl bg-slate-200 border-2 border-slate-300'>
           <input type="text" onChange={(e) => setDonationData({...donationData, title:e.target.value})} placeholder='Your title goes here...' className='outline-none w-5/6 my-4 h-8 rounded-lg p-4'/>
           <textarea 
-            placeholder='Describe your problem and strategy here' 
+            placeholder='Describe the problem you are trying to solve and the strategy here' 
             className='outline-none w-5/6 my-4 rounded-lg p-4' 
             cols={30} rows={8}
             onChange={(e) => setDonationData({...donationData, explanation:e.target.value})}
           />
-          <input type="file" accept='images/*' multiple onChange={selectFiles} hidden id='fileUpload' className='outline-none w-5/6 my-4 bg-white'/>
-          <label htmlFor="fileUpload" className='cursor-pointer h-14 w-14 flex items-center justify-center rounded-[50%] bg-slate-600'></label>
+          <input type="file" accept='images/*' multiple onChange={selectFiles} hidden id='fileUpload'/>
+          <span>Click here to add images</span>
+          <label htmlFor="fileUpload" className='cursor-pointer h-14 w-14 flex items-center justify-center rounded-[50%]'>
+            <img src="assets/add.png" alt="" className='h-full w-full' />
+          </label>
           {donationData.imagesPreview.length > 0 && <div className='w-5/6 flex justify-center overflow-scroll py-8'>
-            {donationData.imagesPreview.map((el, index) => <img key={index} src={el} alt='' className='flex-none h-[100px] w-[100px] rounded-lg overflow-hidden mx-4 bg-gray-300'/>)}
+            {donationData.imagesPreview.map((el, index) => <img key={index} src={el} alt='' className='flex-none h-[80px] w-[80px] rounded-lg overflow-hidden mx-4 bg-gray-300'/>)}
           </div>}
-          <input type="number" placeholder='Your budget amount goes here...' onChange={(e) => setDonationData({...donationData, budgetAmount:Number(e.target.value)})} className='outline-none w-5/6 my-4 h-8 rounded-lg p-4'/>
+          <input 
+            type="number" 
+            placeholder='Your budget amount goes here...' 
+            onChange={(e) => setDonationData({...donationData, budgetAmount:Number(e.target.value)})} 
+            className='outline-none w-5/6 h-8 mt-2 rounded-lg p-4'
+          />
           <div className='w-5/6 h-24 flex items-center justify-center'>
-            { true ? <button className='h-8 w-[150px] rounded-lg text-white bg-blue-500'>Publish</button> : <ConnectButton/>}
+            { true ? <button className='h-8 w-[150px] rounded-lg text-white bg-blue-500 hover:bg-blue-600'>Publish</button> : <ConnectButton/>}
           </div>
         </form>
       </div>
